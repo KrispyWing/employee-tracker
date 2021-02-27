84b1d7888d1da9befcc805f25e415811d41747c1
Kris Pennimpede <kpennimpede@hotmail.com>
2021-02-27 14:41:41 -0500

    can connect and view basic employee table

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const mysql = require('mysql2');
+const inquirer = require('inquirer');
+const cTable = require('console.table');
 require('dotenv').config();
 
 //Create connection for the sql databse
@@ -14,6 +16,50 @@ const connection = mysql.createConnection({
 connection.connect(err => {
   if (err) throw err;
   console.log('connected as id ' + connection.threadId);
-  connection.end()
+  console.log('WELCOME TO THE EMPLOYEE MANAGEMENT SYSTEM')
   //Call to Start main function goes here
+  mainMenu();
 });
+
+function mainMenu () {
+  return inquirer.prompt([
+    {
+      type: 'list',
+      name: 'menu',
+      message: 'What would you like to do?',
+      choices: [
+        'View Departments',
+        'View Roles',
+        'View Employees',
+        'Add Department',
+        'Add Role',
+        'Add Employee',
+        'Update Employee Role'
+      ]
+    }
+  ])
+  .then(function(data) {
+    switch (data.menu) {
+      case "View Departments":
+        viewDept();
+        break;
+      
+      case "View Roles":
+        viewRoles();
+        break;
+
+      case "View Employees":
+        viewEmployees();
+        break;
+    }
+  });
+};
+
+function viewEmployees() {
+  connection.query('SELECT * FROM employees', 
+    function(err, results) {
+      console.table(results);
+    }
+  );
+  connection.end();
+}
