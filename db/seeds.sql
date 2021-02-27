c472bba93abc358c96244695047b6a2bff7326d1
Kris Pennimpede <kpennimpede@hotmail.com>
2021-02-27 13:47:14 -0500

    Create and populate employees table

---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES
+  ('Ronald', 'Firbank', 1, NULL),
+  ('Virginia', 'Woolf', 1, 1),
+  ('Piers', 'Gaveston', 1, NULL),
+  ('Charles', 'LeRoi', 2, 1),
+  ('Katherine', 'Mansfield', 2, 1),
+  ('Dora', 'Carrington', 3, 2),
+  ('Edward', 'Bellamy', 3, 2),
+  ('Montague', 'Summers', 3, NULL),
+  ('Octavia', 'Butler', 3, 2),
+  ('Unica', 'Zurn', 4, NULL);
